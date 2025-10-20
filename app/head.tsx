7dd91fa3cbf7e0620d1f54d381262c8f38599b6a
Siddhant Jain <sja178@sfu.ca>
2025-10-19 22:20:42 -0700

    fix(web): force favicon via custom app/head.tsx links

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Aether</title>
+      <meta name="theme-color" content="#000000" />
+      <link rel="icon" href="/favicon.png" sizes="any" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+    </>
+  )
+}
+
+
